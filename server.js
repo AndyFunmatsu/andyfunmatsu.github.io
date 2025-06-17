2a9f1c9f0ca423123235ab6c42f157243d61d84d
Funmatsu <a_honla@live.concordia.ca>
2025-06-16 23:59:30 -0400

    including esp8266 in the list of authorized origins

--- server.js
+++ server.js
@@ -19,8 +19,14 @@ app.options('/', cors(corsOptions));
 
 app.use(express.json());
 
+const allowedOrigins = ["https://andyfunmatsu.github.io", "https://192.168.0.26"];
+
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://andyfunmatsu.github.io");
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     
